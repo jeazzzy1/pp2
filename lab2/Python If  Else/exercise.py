33fc6e29041c484fedd14edc0ccb2a6d50d03330
jeazzzy1 <156634633+jeazzzy1@users.noreply.github.com>
2024-01-28 06:14:14 +0500

    Add files via upload

---
+++ lab2/Python If  Else/exercise.py
@@ -0,0 +1,60 @@
+#exercise 1. Print "Hello World" if a is greater than b.
+a = 50
+b = 10
+if a > b:
+  print("Hello World")
+
+#exercise 2. Print "Hello World" if a is not equal to b.
+a = 50
+b = 10
+if a != b:
+  print("Hello World")
+
+#exercise 3. Print "Yes" if a is equal to b, otherwise print "No".
+a = 50
+b = 10
+if a == b:
+  print("Yes")
+else:
+  print("No")
+
+#exericse 4.Print "1" if a is equal to b, print "2" if a is greater than b, otherwise print "3".
+a = 50
+b = 10
+if a == b:
+  print("1")
+elif a > b:
+  print("2")
+else:
+  print("3")
+
+#exercise 5.Print "Hello" if a is equal to b, and c is equal to d.
+a = 1
+b = 1
+c = 1
+d = 1
+
+if a == b and c == d:
+  print("Hello") 
+
+#exercise 6. Print "Hello" if a is equal to b, or if c is equal to d.
+if a == b or c == d:
+  print("Hello")
+
+#exercise 7. Complete the code block, print "YES" if 5 is larger than 2.
+#Hint: remember the indentation.
+if 5 > 2:
+  print("YES")
+
+#exericse 8.
+a = 2
+b = 5
+print("YES") if a == b else  print("NO")
+
+#exercise 9.Use an if statement to print "YES" if either a or b is equal to c.
+a = 2
+b = 50
+c = 2
+if a == c or b == c:
+
+  print("YES")
