0eb4d02c5826b9d8d51131ed7e83862998bc3d63
jeazzzy1 <156634633+jeazzzy1@users.noreply.github.com>
2024-01-28 06:03:32 +0500

    Delete lab2 directory

--- lab2/lists/Copy Lists.py
+++
@@ -1,9 +0,0 @@
-#example 1
-thislist = ["apple", "banana", "cherry"]
-mylist = thislist.copy()
-print(mylist)
-
-#example 2
-thislist = ["apple", "banana", "cherry"]
-mylist = list(thislist)
-print(mylist)
