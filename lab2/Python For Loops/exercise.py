f4f120256c361534315f9a82e3623902876bc227
jeazzzy1 <156634633+jeazzzy1@users.noreply.github.com>
2024-01-28 06:04:36 +0500

    Add files via upload

---
+++ lab2/Python For Loops/exercise.py
@@ -0,0 +1,22 @@
+#exercise 1. Loop through the items in the fruits list.
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+#exercise 2. In the loop, when the item value is "banana", jump directly to the next item.
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+#exercise 3. Use the range function to loop through a code set 6 times.
+for x in range(6):
+  print(x)
+
+#exercise 4. Exit the loop when x is "banana".
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
